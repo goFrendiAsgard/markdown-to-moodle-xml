51937f681fbcaccfb4595c2476d772ea706b3786
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-11-14 07:21:39 +0700

    update file

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def convert():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 set -e
 docker build . -t gofrendi/markdown-to-moodle
-docker start --name markdown-to-moodle -p 5000:5000 -d gofrendi/markdown-to-moodle
+docker run -p 5000:5000 --name markdown-to-moodle gofrendi/markdown-to-moodle -d
 docker push gofrendi/markdown-to-moodle
