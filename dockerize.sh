441a72ea30ccb7b142722a68fdf7fd157a3ae6dc
goFrendiAsgard <gofrendiasgard@gmail.com>
2023-11-14 07:06:58 +0700

    update dockerize

--- dockerize.sh
+++ dockerize.sh
@@ -1,4 +1,4 @@
 set -e
 docker build . -t gofrendi/markdown-to-moodle
-docker run gofrendi/markdown-to-moodle -p 5000:5000 -d
+docker start --name markdown-to-moodle -p 5000:5000 -d gofrendi/markdown-to-moodle
 docker push gofrendi/markdown-to-moodle
